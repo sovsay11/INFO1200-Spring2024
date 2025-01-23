484179ba80c35ada3a1ab90978f7c46e81252fa5
Jaronmutombo <kalungamutombo@gmail.com>
2025-01-22 23:12:44 -0700

    Jaronmutombo.py
    I.ve created this copy of the start file.

--- Jaronmutombo.py
+++ Jaronmutombo.py
@@ -1,8 +1,11 @@
 name = "Your name"
 generic_statement = "I like the color..."
 print(name)
-phone = 8019999999
+phone = 1234567890
 print(f”phone: {phone}”)
+uvu = "GO WOLVERINES"
+byu = "GO COUGS"
+
 
 
 
