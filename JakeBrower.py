563c02afb79de82beaa198a03dee4fbb7342e4ec
Jake-J-Brower <Jake.j.brower@gmail.com>
2025-01-22 18:05:59 -0700

    Create JakeBrower.py
    Created a copy of the "Start" file and added print statements with a phone number

---
+++ JakeBrower.py
@@ -0,0 +1,8 @@
+name = "Jake Brower"
+generic_statement = "I love the color blue"
+print ("Hello, my name is")
+print (name)
+print (generic_statement);
+phone = 9114715911
+print ("This is not my phone number")
+print (phone)
