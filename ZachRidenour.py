2d9c0887145756748aa41e345f87148b91c177bd
Zack <zach@ridenourfamily.com>
2025-01-22 18:46:35 -0700

    Zach enters the fray

---
+++ ZachRidenour.py
@@ -0,0 +1,8 @@
+name = "Zach Ridenour"
+generic_statement = "I love the color Teal"
+print ("Why Hello there, my name is Zach.")
+print (name)
+print (generic_statement);
+phone = 123456789
+print ("This really isn't my number")
+print (phone)
